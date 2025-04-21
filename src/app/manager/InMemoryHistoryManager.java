dba54fdc2696c4c15507d0e48c43096990bd8658
Beardgit <sanych.beard@gmail.com>
2025-04-21 19:10:16 +0300

    Добавлены методы добавления в историю и добавлениев linkLast

--- src/app/manager/InMemoryHistoryManager.java
+++ src/app/manager/InMemoryHistoryManager.java
@@ -8,32 +8,62 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private List<Task> history = new ArrayList<>();
 
-    private Map<Integer, Task> historyMap = new HashMap<>();
+    //     Хранение головы и хвоста
     private Node head;
     private Node tail;
 
+    //  Хранение истории
+    private final Map<Integer, Node> historyMap = new HashMap<>();
 
-    @Override
-    public void addToHistory(Task task) {
-        if (history.size() == 10) {
-            history.remove(0);
+    //   Блок обработки Node
+    private void linkLast(Task task) {
+        Node nodeTask = new Node(task);
+        if (head == null && tail == null || historyMap.isEmpty()) {
+            this.head = nodeTask;
+            this.tail = nodeTask;
+        }else {
+            tail.setNext(nodeTask);
+            nodeTask.setPrev(this.tail);
+            tail = nodeTask;
         }
-        history.add(task);
+        historyMap.put(task.getId(), nodeTask);
     }
 
-//    TODO
+    private void removeNode(Node node) {
+       if(node.getPrev() != null){
+           node.getPrev().setNext(node.getNext());
+       }else {
+           this.head = node.getNext();
+       }
+       if(node.getNext() != null){
+           node.getNext().setPrev(node.getPrev());
+       }else {
+           this.tail = node.getPrev();
+       }
+       historyMap.remove(node.getTask().getId());
+    }
+
+    // блок обработки истории
+    @Override
+    public void addToHistory(Task task) {
+        if(task != null){
+            Node nodeTask = historyMap.get(task.getId());
+           if(nodeTask != null){
+               removeNode(nodeTask);
+           }
+            linkLast(nodeTask.getTask());
+        }
+    }
+
     @Override
     public void removeToHistory(Task task) {
-//        !TODO
-        System.out.println("Удаляю!");
+
     }
 
     @Override
     public List<Task> getHistory() {
-        System.out.println(history);
-        return new ArrayList<>(history);
+
     }
 
 }
--- src/app/manager/Node.java
+++ src/app/manager/Node.java
@@ -3,13 +3,28 @@ package app.manager;
 import app.tasks.Task;
 
 public class Node {
-    private Task task;
-    private Task prev;
-    private Task next;
 
-    public Node(Task task) {
+    Node prev;
+    Node next;
+    Task task;
+
+    Node(Task task) {
         this.task = task;
+    }
+
+    public Node getPrev() {
+        return this.prev;
+    }
+
+    public void setPrev(Node prev) {
         this.prev = prev;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node next) {
         this.next = next;
     }
 
@@ -17,13 +32,7 @@ public class Node {
         return task;
     }
 
-    public Task getPrev() {
-        return prev;
+    public void setTask(Task task) {
+        this.task = task;
     }
-
-    public Task getNext() {
-        return next;
-    }
-
-
 }
