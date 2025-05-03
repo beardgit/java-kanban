090b23632f77b65d95f7839b132242b71dfd1b37
Beardgit <sanych.beard@gmail.com>
2025-05-03 07:40:11 +0300

    Исправить опечатку нащвания класса TaskIntersectionTest, инициализация TaskManager в данном классе через @BedoreEach

--- test/app/manager/TaskIntersectionTest.java
+++ test/app/manager/TaskIntersectionTest.java
@@ -1,6 +1,7 @@
 package app.manager;
 
 import app.tasks.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -8,10 +9,17 @@ import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TaskIntersectiontest {
+public class TaskIntersectionTest {
+
+    TaskManager taskManager;
+
+    @BeforeEach
+    void init() {
+        taskManager = new InMemoryTaskManager(new InMemoryHistoryManager());
+    }
+
     @Test
     public void shouldThrowExceptionOnTimeIntersection() {
-        TaskManager taskManager = new InMemoryTaskManager(new InMemoryHistoryManager());
 
         Task task1 = new Task("Task 1", "Description", Instant.parse("2023-10-01T10:00:00Z"), Duration.ofMinutes(30));
         Task task2 = new Task("Task 2", "Description", Instant.parse("2023-10-01T10:15:00Z"), Duration.ofMinutes(30));
@@ -23,12 +31,11 @@ public class TaskIntersectiontest {
 
     @Test
     public void shouldNotThrowExceptionOnNonIntersectingTasks() {
-        TaskManager taskManager = new InMemoryTaskManager(new InMemoryHistoryManager());
 
         Task task1 = new Task("Task 1", "Description", Instant.parse("2023-10-01T10:00:00Z"), Duration.ofMinutes(30));
         Task task2 = new Task("Task 2", "Description", Instant.parse("2023-10-01T11:00:00Z"), Duration.ofMinutes(30));
 
         taskManager.appendTask(task1);
-        taskManager.appendTask(task2); // Должно работать без исключений
+        taskManager.appendTask(task2);
     }
 }
